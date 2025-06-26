a40009d37d3e4609907ac30c358aa12d1b65e439
Nathan Yee <yee.nathan.c@gmail.com>
2025-06-26 13:41:02 -0700

    add tf output for iceberg setup job

--- infrastructure/terraform/src/constructs/data-processing-construct/outputs.tf
+++ infrastructure/terraform/src/constructs/data-processing-construct/outputs.tf
@@ -7,3 +7,8 @@ output "game_events_etl_iceberg_job" {
   description = "ETL Job for processing game events into optimized format for analytics"
   value       = aws_glue_job.game_events_etl_iceberg_job.name
 }
+
+output "iceberg_setup_job" {
+  description = "Glue Job for setting up new iceberg table"
+  value       = var.enable_apache_iceberg_support ? aws_glue_job.iceberg_setup_job[0].name : ""
+}
