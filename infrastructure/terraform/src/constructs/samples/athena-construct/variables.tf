3d3ba2d124d6541fa84d481bc7b07b690cd08d3d
Christian Orellana <corellav@amazon.com>
2026-05-11 11:59:10 -0400

    Gate Athena sample queries on ENABLE_APACHE_ICEBERG_SUPPORT

--- infrastructure/terraform/src/constructs/samples/athena-construct/variables.tf
+++ infrastructure/terraform/src/constructs/samples/athena-construct/variables.tf
@@ -1,11 +1,16 @@
 variable "events_database" {
-  type        = string
+  type = string
 }
 
 variable "game_events_workgroup" {
-  type        = string
+  type = string
 }
 
 variable "raw_events_table" {
-  type        = string
+  type = string
+}
+
+variable "enable_apache_iceberg_support" {
+  type        = bool
+  description = "When true, deploy queries targeting the Iceberg schema (native event_timestamp). When false, deploy legacy Hive-partitioned queries (year/month/day partition columns)."
 }
